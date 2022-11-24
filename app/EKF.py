5b9d00fd6bcde9c59a6a1faa4739c39574251088
AdrienPannatier <adrien.pannatier@epfl.ch>
2022-11-24 09:36:52 +0100

    feat: filtering cleaning

--- app/EKF.py
+++ app/EKF.py
@@ -3,6 +3,9 @@ from app.state import state, State
 
 """ THIS IS A NEW VERSION OF THE FILTER KALMAN.PY AND FILTERING:PY ARE OBSELETE """
 
+DIAMETER = 9,5
+
+
 class ExtendedKalmanFilter(object):
     """ 
     class containing every necessary function for EKF
@@ -46,22 +49,28 @@ class ExtendedKalmanFilter(object):
                           [0, 1, 0],
                           [0, 0, 1]])
 
-        self.update_B()
+        self.B = np.matrix([[np.cos(self.E[2])*self.dt, 0],
+                           [np.sin(self.E[2])*self.dt, 0],
+                           [0, self.dt]])
 
-        self.update_U(0, 0)
+        self.U = np.matrix(0, 0)
 
-        self.update_G()
+        self.G = np.matrix([[1, 0, -np.sin(self.E[2])*self.dt*self.U[0]],
+                            [0, 1, np.cos(self.E[2])*self.dt*self.U[0]],
+                            [0, 0, 1]])
 
         # Matrice d'observation, on observe que x et y
         self.H=np.matrix([[1, 0, 0],
-                          [0, 1, 0]])
+                          [0, 1, 0],
+                          [0, 0, 1]])
 
         self.Q=np.matrix([[1, 0, 0],
                           [0, 1, 0],
                           [0, 0, 1]]) # valeurs uniquement sur la diag, les bruits sont indépendants 
 
-        self.R=np.matrix([[1, 0],
-                          [0, 1]]) # bruit de la caméra, peut-être donné par le constructeur.  sinon tuner
+        self.R=np.matrix([[1, 0, 0],
+                          [0, 1, 0],
+                          [0, 0, 1]]) # bruit de la caméra, peut-être donné par le constructeur.  sinon tuner
 
         self.P=np.eye(self.A.shape[1])
 
@@ -81,7 +90,7 @@ class ExtendedKalmanFilter(object):
         param speedR: right wheel speed sensor in cm/s
         """
         speed_forward = (speedL+speedR)/2
-        speed_rotation = (speedL-speed_R)/2/30/5   
+        speed_rotation = (speedL-speed_R)/DIAMETER
         self.U = np.matrix([speed_forward], [speed_rotation])
 
     def update_G(self):
@@ -92,17 +101,15 @@ class ExtendedKalmanFilter(object):
                             [0, 1, np.cos(self.E[2])*self.dt*self.U[0]],
                             [0, 0, 1]])
 
-    def predict(self, speedL, speedR, orientation):
+    def predict(self, speedL, speedR):
         """ 
         estimates the next state
 
         param speedL: left wheel speed sensor in cm/s
         param speedR: right wheel speed sensor in cm/s
-        param orientation: orientation from x axis in rad
 
         return E: estimated state
         """
-        #MUST UPDATE ORIENTATION BY OFFSET WITH THE MOTORS------------------------------------------------------------------------------------------
 
         #updates the matrices B,U,G
         self.update_B()
@@ -113,13 +120,13 @@ class ExtendedKalmanFilter(object):
         self.E=np.dot(self.A, self.E) + np.dot(self.B,self.U)
         # Calcul de la covariance de l'erreur
         self.P=np.dot(np.dot(self.G, self.P),self.G.T)+self.Q
-        return self.E
+        return self.E[0], self.E[1], self.E[2]
 
     def update(self, z):
         """ 
         updates the estimation matrices when camera data is given
 
-        param z: x,y position in cm
+        param z: x,y,orientation in [cm][cm][rad]
 
         return E: estimated state
         """        
@@ -127,10 +134,10 @@ class ExtendedKalmanFilter(object):
         # Calcul du gain de Kalman
         S=np.dot(self.H, np.dot(self.P, self.H.T))+self.R
         K=np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
-
+         
         # Correction / innovation
         self.E=np.round(self.E+np.dot(K, (z-np.dot(self.H, self.E))))
         I=np.eye(self.H.shape[1])
         self.P=(I-(K*self.H))*self.P
 
-        return self.E
+        return self.E[0], self.E[1], self.E[2]
--- app/filtering.py
+++ app/filtering.py
@@ -1,104 +1,42 @@
-from app.state import state, update_state
-from app.kalman import kalman_filter_1D
+from tdmclient import ClientAsync, aw
+import math
+import numpy as np
 
-""" OBSELETE """
+from app.state import state
+from app.EKF import ExtendedKalmanFilter
 
-# idea: have a thymio_data_speed array to store the speed of the motors since the beggining
-thymio_data_speed = []
-states_data = [] # containing states (x pos, y pos, orientation)
-Ts = 0.1 # scan period
+THYMIO_TO_CM = 3.85/100 #NEEDS TO BE MODIFIED ACCORDING TO THE THYMIO
 
-last_cam_state = State(
-    position=(0.5, 1.4),
-    orientation=(math.pi/2),
-    start=(0.4, 0.4),
-    end=(1.6, 1.6),
-    physical_size=(BOARD_SIZE_M, BOARD_SIZE_M),
-    path=[],
-    obstacles=[],
-    computation_time=0.0
-) #INITIAL STATE FOR DECLARATION
+#doing only once as a declaration (maybe to move elswere)
+ekf = ExtendedKalmanFilter(0, 0, math.pi()/2)
 
-x_est = 0 # initial x position
-Px_est = [10 * np.ones(2)] #estimation de a quel point la caméra a raison
-
-y_est = 0 # initial y position
-Py_est = [10 * np.ones(2)]
-
-# to compute beforehand
-q_nu = std_speed/2 # variance on speed state
-r_nu = std_speed/2 # variance on speed measurement 
-
-qp = 0.04 # variance on position state
-rp = 0.25 # variance on position measurement 
-
-
-def get_speed_x_y(orientation, speed_left, speed_right):
-    """ decomposes the speed in the axis base
-    
-        param orientation: orientation of the thymio in [rad] (0 is facing east)
-        param speed_left: speed given by the left motor in thymio units
-        param speed_right: speed given by the right motor in thymio units
-        
-        return speed_x: forward speed along the x axis
-        return speed_y: forward speed along the y axis
-    """
-    sinor = math.sin(orientation)
-    cosor = math.cos(orientation)
-
-    forward_speed = (speed_left + speed_right) / 2
-
-    speed_x = cosor * forward_speed
-
-    speed_y = sinor * forward_speed
-
-    return speed_x, speed_y
-
-
-def filtering(current_state, speed_left, speed_right):
+def filtering(current_state):# current state is a class
 
     """    Runs the filtering pipeline once, must be called each time a state is measured
         
         param current_state: current state the thymio is in from vision
-            containing: x position [cm], y position [cm], orientation [rad]
-        param speed_left: speed given by the left motor in cm/s units
-        param speed_right: speed given by the right motor in cm/s thymio units
+            containing: x position [cm], y position [cm], orientation [rad] 
         
-        return state_est: estimmation of the state
+        return state: estimmation of the state (is there realy an use for that?)
     """   
-
-    # update orientation
-    rotation_speed = (speed_left - speed_right) /2 /30 /5
-    diffOr = rotation_speed * Ts
-    orientation_new = orientation + diffOr
-    if (orientation_new > 2*math.pi):
-        orientation_new = orientation_new - 2*math.pi
-
-    # local variables
-    speed_x, speed_y = get_speed_x_y(orientation_new, speed_left, speed_right)
-
-    (cam_previous_pos_x, cam_previous_pos_y) = last_cam_state.position
-    (cam_pos_x, cam_pos_y) = current_state.position
-
-    # Filtering
-    new_x_est, new_Px_est = kalman_filter_1D(speed_x, cam_previous_pos_x, cam_pos_x, x_est, Px_est, orientation_new)
-    x_est = new_x_est # x estimmation
-    Px_est = new_Px_est
-
-    new_y_est, new_Py_est = kalman_filter_1D(speed_y, cam_previous_pos_y, cam_pos_y, y_est, Py_est, orientation_new)
-    y_est = new_y_est # y estimmation
-    Py_est = new_Py_est
-
-    last_cam_state.update_state_position(current_state.position) #TO IMPLEMENT IN STATE
     
-    #create output state
-    state_est = State()
-    state_est.position = (x_est, y_est)
-    state_est.orientation = orientation_new
+    speedL = node["motor.left.speed"] #in thymio units
+    speedR = node["motor.right.speed"] #in thymio units
 
-    #update global state
-    state.position = (x_est, y_est)
-    state.orientation = orientation_new
+    #speed transformation from thymio units to cm/s
+    speedL = speedL * THYMIO_TO_CM
+    speedR = speedR * THYMIO_TO_CM
+
+    #blind estimation of the next state
+    state.position[0], state.position[1], state.orientation = ekf.predict(speedL, speedR)
+
+    #if camera reading, estimation of the next state with measurement
+    pose = np.matrix([current_state.position[0], current_state.position[1], current_state.orientation])
+
+    if(pose[0] != 0 and pose[1] != 0): #verify condition (not sure)---------------------------------------------------------------------------------------------------------------
+        KF.update(pose)
+
+    #send global state
     state.mark_stale()
 
-    return state_est
+    return state
